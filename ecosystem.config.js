cf22d8b921572c9e529d446a2184388fccf6ff4c
Ioannis Mertzanis <jomertz@gmail.com>
2017-01-13 23:04:54 +0200

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'lawdb',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-35-166-122-24.us-west-2.compute.amazonaws.com',
+      key: '~/Downloads/Acer-laptop.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:imertz/lawdb.git',
+      path: '/home/ubuntu/lawdb',
+      'forward-agent': 'yes',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
