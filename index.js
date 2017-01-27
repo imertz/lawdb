cebe1f8bc2b0b8400cb9b28122d1f4dcfc1ddcb2
Ioannis Mertzanis <jomertz@gmail.com>
2017-01-28 00:22:29 +0200

    Changed index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
