8242dc0eac7462eef30ea3e5b6afa67371e2cb7a
Ioannis Mertzanis <jomertz@gmail.com>
2017-02-11 01:58:17 +0200

    Updated config database

--- config/main.js
+++ config/main.js
@@ -1,8 +1,8 @@
 
-let config = {  
+let config = {
   // Secret key for JWT signing and encryption
   'secret': 'passphrase',
-  'database': 'mongodb://localhost:27017/officeDB'
+  'database': 'mongodb://jomertz:hydra83@localhost:2727/officeDB'
 }
 
 export {config}
